06d5bfa516418214f93b8135dda8140d4bb93dad
rodney-wang <wfei@aibee.com>
2019-06-14 00:15:24 -0700

    print out log info

--- utils/crop_plate_and_generate_ocrlabel.py
+++ utils/crop_plate_and_generate_ocrlabel.py
@@ -35,7 +35,7 @@ def parse_ocr_str(ocr_str):
     ocr_chars = ocr_chars + digits
 
     ocr = ''.join(ocr_chars)
-    return ocr
+    return ocr, pp
 
 
 def write_plate_image(img_path, out_dir, label_txt):
@@ -46,7 +46,6 @@ def write_plate_image(img_path, out_dir, label_txt):
     If there is multiple rows in a plate, (like hongkong plate),
     write multiple plate images into files.
     """
-    corner = json.load(open(json_path))
 
     car_images = glob.glob(img_path + '*/*.jpg')
     if not os.path.exists(out_dir):
@@ -56,16 +55,19 @@ def write_plate_image(img_path, out_dir, label_txt):
     for i, image_name in enumerate(car_images):
         if i >5:
             continue
+        if i%1000 == 0:
+            print('### {}: Processing {}'.format(i, bname.encode('utf8')))
+
         fname_split = os.path.basename(image_name).split('_')
         if len(fname_split) <6:
             continue
         corner_str, ocr_str = fname_split[3], fname_split[4]
         corners = parse_corner_str(corner_str)
-        ocr     = parse_ocr_str(ocr_str)
-
-        print('### {}: Processing {}'.format(i, bname.encode('utf8')))
+        ocr, pp = parse_ocr_str(ocr_str)
+        if pp == 0:
+            continue
         if len(corners) < 8:
-            print('{}: {} length of pts shorter than 8'.format(i, image_name))
+            print('{}: {} length of corner pts shorter than 8'.format(i, image_name))
             continue
         img = imread(image_name)
         if img is None:
@@ -75,7 +77,6 @@ def write_plate_image(img_path, out_dir, label_txt):
         cv2.imwrite(out_path, plate_img)
 
         plate_ocr = ocr.strip()
-        plate_ocr = plate_ocr.replace(' ', '')
         plate_label = '|' + '|'.join(plate_ocr.decode('utf8')) + '|'
         line = out_path + ';' + plate_label + '\n'
         print line
@@ -86,11 +87,11 @@ def write_plate_image(img_path, out_dir, label_txt):
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Plate end to end test')
-    parser.add_argument('--img_dir', default='/ssd/wfei/data/plate_for_label/hongkong/hkia_car_crop/caffe_crop/',
+    parser.add_argument('--img_dir', default='/ssd/wfei/data/ccpd/home/booy/booy/ccpd_dataset',
                         type=str, help='Input test image dir')
-    parser.add_argument('--plate_dir', default='/ssd/wfei/data/plate_for_label/hongkong/plates/',
+    parser.add_argument('--plate_dir', default='/ssd/wfei/data/ccpd/plates/',
                         type=str, help='Output plate image dir')
-    parser.add_argument('--label_txt', default='/ssd/wfei/data/plate_for_label/hongkong/hkia_car_ocrlabel.txt',
+    parser.add_argument('--label_txt', default='/ssd/wfei/data/ccpd/ccpd_selected_ocrlabel.txt',
                         type=str, help='Output OCR label txt')
     args = parser.parse_args()
     return args
